2a32295b9e655e2d0cbaebac10fbf8232cb5f8ef
Ben Krieger <blitzrk@gmail.com>
2016-03-28 21:37:22 -0600

    Parse asciidoc header instead of pseudo front matter

--- post.js
+++ post.js
@@ -10,32 +10,33 @@ function extract(file) {
   if(file.isNull()) return {};
   if(file.isStream()) throw new Error('Does not support file streams');
 
-  var header = {};
   var contents = file.contents.toString();
-  if(contents.startsWith("<<<")) {
-    var lines = contents.split("\n").slice(1);
-    header = lines.slice(0, lines.indexOf('<<<'))
-      .map(function(s){ return s.split(":") })
-      .reduce(function(acc, arr) {
-        acc[arr[0]] = arr[1].trim();
-        return acc;
-      }, {});
-    contents = lines.slice(lines.indexOf('<<<')+1).join('\n');
-  }
+  var header = contents.split("\n\n", 1)[0].split('\n')
+    .filter(function(line) { return ! line.startsWith(':'); });
 
-  file.contents = new Buffer(contents);
-  return header;
+  header = {
+    title: header[0] && header[0].startsWith('= ') && header[0].substring(2, 
+      header[0].includes(':') ? header[0].lastIndexOf(':') : header[0].length).trim(),
+    author: header[1] && header[1].split(';')[0].split('<')[0].trim(),
+    email: header[1] &&
+      header[1].substring(header[1].indexOf('<')+1, header[1].indexOf('>')).trim(),
+    date: header[2] && header[2].substring(
+        header[2].startsWith('v') ? header[2].indexOf(',') + 1 : 0,
+        header[2].includes(':') ? header[2].indexOf(':') : header[2].length).trim()
+  };
+
+  return header
 }
 
 function header(file) {
   var post = extract(file) || {}
+  post.date = moment(post.date).isValid() ? moment(post.date) : undefined
+
   if(! post.title || ! post.author || ! post.date) {
     throw new Error(file.relative + " requires a header with title, author, and date");
   }
 
   post.slug = slug(post.title);
-  post.date = moment(post.date);
-
   file._post = post;
   return file;
 }

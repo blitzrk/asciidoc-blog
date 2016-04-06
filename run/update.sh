c012ff27f4e4a36ef0285a2627e838bac0eb5666
Ben Krieger <blitzrk@gmail.com>
2016-04-06 17:59:36 -0600

    Accept all changes in update script

--- run/update.sh
+++ run/update.sh
@@ -5,5 +5,5 @@ if ! git remote | grep -q ^fork$; then
 	git remote add fork https://github.com/blitzrk/asciidoc-blog.git
 fi
 
-git pull --squash fork master
+git pull --squash -s recursive -X theirs fork master
 git commit -m "Update asciidoc-blog"

176935cf3f98173b963d7796fb4863837acde7a4
Ben Krieger <blitzrk@gmail.com>
2016-04-21 12:46:13 -0600

    Only patch when necessary

--- run/patch.sh
+++ run/patch.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
+set -euo pipefail
+dir=`dirname "$(readlink -f "$0")"`
 
-find ./node_modules -name index.js -type f | grep 'css/lib/parse' | xargs -n 1 sed -i "582a\\
+function get_file() {
+	find "$dir/../.." -name index.js -type f \
+		| grep 'css/lib/parse'
+}
+
+function needs_patching() {
+	file="$1"
+	line=`sed '583q;d' "$file"`
+	[[ "$line" != "if (typeof obj !== 'string') {" ]]
+}
+
+function do_patch() {
+	file="$1"
+
+	sed -i "582a\\
 if (typeof obj !== 'string') {\\
-"
-find ./node_modules -name index.js -type f | grep 'css/lib/parse' | xargs -n 1 sed -i "591a\\
+" "$file"
+
+	sed -i "591a\\
 }\\
-"
+" "$file"
+}
+
+function main() {
+	file="$(get_file)"
+
+	if needs_patching "$file"; then
+		do_patch "$file"
+	fi
+}
+
+main
